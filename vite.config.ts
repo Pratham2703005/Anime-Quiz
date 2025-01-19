8ce523715784c2cffe37aa696cc10e1061600435
Pratham Israni <pk2732004@gmail.com>
2025-01-19 06:13:35 +0530

    run build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separate third-party libraries into their own chunk
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Set to a higher limit, e.g., 1000 KB
+    
+  }
 })
